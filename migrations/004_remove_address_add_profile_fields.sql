b1d2068918f48459ad13790cc1a73ff9e19b4e59
TeleginSergey <telegins125@gmail.com>
2026-05-06 20:27:25 +0300

    Исправить миграцию для обработки существующих NULL значений

--- migrations/004_remove_address_add_profile_fields.sql
+++ migrations/004_remove_address_add_profile_fields.sql
@@ -1,9 +1,13 @@
--- Удаляем поле адреса из таблицы users и добавляем поля полного имени и телефона
+-- Удаляем поле адреса из таблицы users
 ALTER TABLE users DROP COLUMN IF EXISTS users_address;
 
 -- Добавляем поля полного имени и телефона если их нет
 ALTER TABLE users ADD COLUMN IF NOT EXISTS users_full_name VARCHAR(255);
 ALTER TABLE users ADD COLUMN IF NOT EXISTS users_phone VARCHAR(50) NOT NULL DEFAULT '';
+
+-- Обновляем существующие NULL значения перед добавлением ограничений
+UPDATE users SET users_full_name = '' WHERE users_full_name IS NULL;
+UPDATE users SET users_phone = '' WHERE users_phone IS NULL;
 
 -- Добавляем ограничения для обязательных полей
 ALTER TABLE users ALTER COLUMN users_full_name SET NOT NULL;
